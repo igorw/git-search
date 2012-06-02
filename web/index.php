53d555d97338919cea43b062fd26779aae784837
Igor Wiedler <igor@wiedler.ch>
2012-06-02 02:20:38 +0200

    fooling around with elasticsearch

---
+++ web/index.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app['elastica'] = $app->share(function () {
+    return new Elastica_Client();
+});
+
+$app->get('/', function (Request $request) use ($app) {
+    $query = $request->query->get('q');
+
+    $index = $app['elastica']->getIndex('git-search');
+    $results = $index->search($query);
+
+    $view = 'index.html.twig';
+    if ($request->isXmlHttpRequest()) {
+        $view = 'results.html.twig';
+    }
+
+    return $app['twig']->render($view, array(
+        'query' => $query,
+        'results' => $results,
+    ));
+});
+
+$app->run();
